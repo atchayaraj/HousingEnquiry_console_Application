a84c92068658d488c8504c86d010526399cbed18
atchayaraj <72649598+atchayaraj@users.noreply.github.com>
2022-05-16 17:37:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,22 @@
 import java.util.*;
 public class Main {
 	
-		static String who;
-		static Scanner scan = new Scanner(System.in);
+	static String who;
+	static Scanner scan = new Scanner(System.in);
 	    
-	    public static void main(String[] args)
-	    {
-	    	System.out.println("Welcome to our Housing Enquiry page!\n\n");
+	public static void main(String[] args)
+        {
+	    System.out.println("Welcome to our Housing Enquiry page!\n\n");
 	       
-	        while(true)
-	        {
-	            System.out.println("Enter that you're a Tenant or an Owner\n");
+	    while(true)
+	    {
+	        System.out.println("Enter that you're a Tenant or an Owner\n");
 	            
-	            who=scan.nextLine();
+	        who=scan.nextLine();
 	            
-	            if(who.equals("Owner"))
-	            {
-	                new user().getstatus();
+	        if(who.equals("Owner"))
+		{
+			new user().getstatus();
 	                System.out.println();
 	               
 	            }
